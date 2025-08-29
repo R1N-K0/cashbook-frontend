0f1b8d7af2b2a4a197ec0bd676921f31db5076b0
R1N-K0 <peanut904018@gmail.com>
2025-08-29 11:27:51 +0900

    feat: フィルターの実装

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 import { Button } from '@/components/ui/button'
 import CategoriesList from '@/features/category/components/CategoriesList'
+import FilterBox from '@/features/components/FilterBox'
 import SearchBox from '@/features/components/SearchBox'
 import { useState } from 'react'
 
 const CategoryPage = () => {
   const [keyword, setKeyword] = useState<string>('')
+  const [filter, setFilter] = useState<string>('')
+
   return (
     <>
       <div className="container-fluid px-8 py-8 flex flex-col space-y-5">
@@ -19,7 +22,12 @@ const CategoryPage = () => {
         </div>
 
         <div className="flex flex-row items-center justify-end gap-5">
-          <div>フィルター</div>
+          <FilterBox
+            placeholder="フィルター"
+            values={['食費', '学費', '治療費']}
+            setFilter={setFilter}
+          />
+
           <SearchBox setState={setKeyword} placeholder="カテゴリーを検索" />
         </div>
 
