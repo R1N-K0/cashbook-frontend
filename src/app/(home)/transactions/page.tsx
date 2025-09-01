ac8f80fd82b373ac3eae602027aae4a754b7a008
1F10230014 <s1f102300144@iniad.org>
2025-09-01 15:40:12 +0900

    add:取引一覧ページ作成

--- src/app/(home)/transactions/page.tsx
+++ src/app/(home)/transactions/page.tsx
@@ -2,28 +2,32 @@
 import Calendar22 from '@/features/components/DateOfBirthPicker'
 import FilterBox from '@/features/components/FilterBox'
 import SearchBox from '@/features/components/SearchBox'
+import { DataTableDemo } from '@/features/transactions/components/DataTable'
 import { useState } from 'react'
 
 const TarnsactionPage = () => {
   const [keyword, setKeyWord] = useState<string>('')
   const [filte, setFilter] = useState<string>('')
   return (
-    <div className="grid grid-rows-[auto_1fr] h-full">
-      <div className="flex flex-row items-center gap-5 p-4">
-        <Calendar22 />
-        <FilterBox
-          placeholder="カテゴリー"
-          values={['食費', '学費', '治療費']}
-          setFilter={setFilter}
-        />
-        <FilterBox
-          placeholder="フィルター"
-          values={['食費', '学費', '治療費']}
-          setFilter={setFilter}
-        />
-        <SearchBox setState={setKeyWord} placeholder="検索" />
+    <div className="grid grid-rows-[auto_1fr] h-full p-8">
+      <div>
+        <div className="text-3xl font-bold text-gray-700">取引一覧</div>
+        <div className="flex flex-row items-center gap-5 p-4">
+          <Calendar22 />
+          <FilterBox
+            placeholder="カテゴリー"
+            values={['収入', '支出']}
+            setFilter={setFilter}
+          />
+          <FilterBox
+            placeholder="フィルター"
+            values={['食費', '学費', '治療費']}
+            setFilter={setFilter}
+          />
+          <SearchBox setState={setKeyWord} placeholder="検索" />
+        </div>
       </div>
-      <div>表</div>
+      <DataTableDemo />
     </div>
   )
 }
