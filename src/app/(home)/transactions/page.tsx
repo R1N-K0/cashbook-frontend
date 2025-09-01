4bfc15e38367b29e8ffc0c1be7eddb712280afbb
1F10230014 <s1f102300144@iniad.org>
2025-09-01 13:02:37 +0900

    カレンダー

--- src/app/(home)/transactions/page.tsx
+++ src/app/(home)/transactions/page.tsx
@@ -1,5 +1,31 @@
+'use client'
+import Calendar22 from '@/features/components/DateOfBirthPicker'
+import FilterBox from '@/features/components/FilterBox'
+import SearchBox from '@/features/components/SearchBox'
+import { useState } from 'react'
+
 const TarnsactionPage = () => {
-  return <div>取引一覧</div>
+  const [keyword, setKeyWord] = useState<string>('')
+  const [filte, setFilter] = useState<string>('')
+  return (
+    <div className="grid grid-rows-[auto_1fr] h-full">
+      <div className="flex flex-row items-center gap-5 p-4">
+        <Calendar22 />
+        <FilterBox
+          placeholder="カテゴリー"
+          values={['食費', '学費', '治療費']}
+          setFilter={setFilter}
+        />
+        <FilterBox
+          placeholder="フィルター"
+          values={['食費', '学費', '治療費']}
+          setFilter={setFilter}
+        />
+        <SearchBox setState={setKeyWord} placeholder="検索" />
+      </div>
+      <div>表</div>
+    </div>
+  )
 }
 
 export default TarnsactionPage
