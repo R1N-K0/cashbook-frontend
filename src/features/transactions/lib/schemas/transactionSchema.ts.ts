64155ff8c06a837c3e8b8033a6aed82d0c0ebe85
R1N-K0 <peanut904018@gmail.com>
2025-09-02 13:03:03 +0900

    feat: エラーメッセージの追加

--- src/features/transactions/lib/schemas/transactionSchema.ts.ts
+++ src/features/transactions/lib/schemas/transactionSchema.ts.ts
@@ -1,13 +1,18 @@
 import z from 'zod'
 
 export const transactionSchema = z.object({
-  categoryId: z.number().min(0),
-  date: z.date(),
-  description: z.string().max(255).nonempty(),
+  categoryId: z.number({ message: 'カテゴリーを選択してください' }).min(0),
+  date: z.date({ message: '取引日を指定してください' }),
+  description: z
+    .string({ message: '取引内容を入力してください' })
+    .max(255)
+    .nonempty(),
   memo: z.string().nullable(),
-  amount: z.number().min(1, { message: '正しい金額を指定してください' }),
+  amount: z
+    .number({ message: '金額を指定してください' })
+    .min(1, { message: '金額を指定してください' }),
   createdUser: z
-    .string()
+    .string({ message: '担当者を入力してください' })
     .nonempty({ message: '担当者を入力してください' })
     .max(255),
 })
