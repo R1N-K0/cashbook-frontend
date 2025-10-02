583a396922f3756d106f027336ab3347d3265aed
1F10230014 <s1f102300144@iniad.org>
2025-10-02 17:30:46 +0900

    patchAPI追記

--- src/app/api/transactions/[id]/route.ts
+++ src/app/api/transactions/[id]/route.ts
@@ -38,3 +38,40 @@ export async function GET(
     )
   }
 }
+
+export async function PATCH(
+  request: Request,
+  context: { params: Promise<{ id: string }> },
+) {
+  const cookieStore = await cookies()
+  const accessToken = cookieStore.get('access_token')?.value
+  const { id } = await context.params
+  try {
+    const body = await request.json()
+    const res = await fetch(`http://localhost:3001/transactions/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        ...(accessToken ? { Cookie: `access_token=${accessToken}` } : {}),
+      },
+      body: JSON.stringify(body),
+    })
+
+    if (!res.ok) {
+      const errorData = await res.json().catch(() => ({}))
+      console.error(errorData)
+      return NextResponse.json(
+        {
+          message: errorData.message ?? '不明なエラーが発生しました',
+        },
+        { status: res.status },
+      )
+    }
+    return NextResponse.json(await res.json())
+  } catch (error) {
+    return NextResponse.json(
+      { message: (error as Error).message ?? '不明なエラーが発生しました' },
+      { status: 500 },
+    )
+  }
+}
