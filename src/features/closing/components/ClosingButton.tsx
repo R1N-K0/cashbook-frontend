a412ad74bf4c67068b9f3857ca452bcf83d152a1
R1N-K0 <peanut904018@gmail.com>
2025-10-02 14:54:05 +0900

    feat: エラー画面の修正

--- src/features/closing/components/ClosingButton.tsx
+++ src/features/closing/components/ClosingButton.tsx
@@ -28,8 +28,10 @@ export default function ClosingButton() {
   const onClick = async () => {
     const res = await executeClosing({ year, month })
 
-    if (!res.success) toast.error(res.message ?? '不明なエラーが発生しました')
-    else {
+    if (!res.success) {
+      if (res.status === 404) toast.info('対象のデータはありませんでした')
+      else toast.error(res.message ?? '不明なエラーが発生しました')
+    } else {
       mutate()
       toast.success('月末締めを実行しました')
     }
