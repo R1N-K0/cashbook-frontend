15c2f3932c6d2f29a1dd1155949c41e7195b0223
1F10230014 <s1f102300144@iniad.org>
2025-10-22 15:29:53 +0900

    fix: texrAreaエラー修正

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,7 +1,13 @@
 import { cn } from '@/lib/utils'
 import * as React from 'react'
 
-function Textarea({ className, ...props }: React.ComponentProps<'textarea'>) {
+function Textarea({
+  className,
+  value,
+  ...props
+}: React.ComponentProps<'textarea'>) {
+  const normalizedValue = value === null ? '' : value
+
   return (
     <textarea
       data-slot="textarea"
@@ -9,6 +15,7 @@ function Textarea({ className, ...props }: React.ComponentProps<'textarea'>) {
         'border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
         className,
       )}
+      value={normalizedValue ?? undefined}
       {...props}
     ></textarea>
   )
