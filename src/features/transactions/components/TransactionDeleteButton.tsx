b33b3ff62b39a099271b24d1dcfc991448b25f1e
R1N-K0 <peanut904018@gmail.com>
2025-10-23 11:56:57 +0900

    fix delete button

--- src/features/transactions/components/TransactionDeleteButton.tsx
+++ src/features/transactions/components/TransactionDeleteButton.tsx
@@ -39,8 +39,20 @@ export default function TransactionDeleteButton({ id, editable }: Props) {
       {editable ? (
         <AlertDialog>
           <AlertDialogTrigger>
-            <div className="p-2  rounded-sm hover:bg-gray-100 text-gray-500 cursor-pointer">
-              削除
+            <div className="group py-1 px-2 pe-3 flex flex-row items-center justify-items-center space-x-1   hover:bg-gray-200 cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-5 h-auto"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="#000000"
+                  d="M3.94 5L2.22 3.28a.75.75 0 1 1 1.06-1.06l18.5 18.5a.75.75 0 0 1-1.06 1.06l-2.19-2.19A3.751 3.751 0 0 1 15.025 22H8.974a3.75 3.75 0 0 1-3.733-3.389L4.07 6.5H2.75a.75.75 0 0 1 0-1.5h1.19Zm13.338 13.34L15 16.06v1.19a.75.75 0 0 1-1.5 0v-2.69l-3-3v5.69a.75.75 0 0 1-1.5 0v-7.19L5.59 6.652l1.144 11.816a2.25 2.25 0 0 0 2.24 2.033h6.052a2.25 2.25 0 0 0 2.24-2.033l.012-.128ZM13.5 10.318l1.5 1.5V9.75a.75.75 0 0 0-1.5 0v.569ZM18.424 6.5l-.771 7.971l1.373 1.374l.905-9.345h1.319a.75.75 0 0 0 0-1.5H15.5a3.5 3.5 0 1 0-7 0h-.318l1.5 1.5h8.742ZM14 5h-4a2 2 0 1 1 4 0Z"
+                />
+              </svg>
+              <span className="text-gray-500 opacity-0 group-hover:opacity-100  transition-opacity duration-300">
+                削除
+              </span>
             </div>
           </AlertDialogTrigger>
           <AlertDialogContent>
